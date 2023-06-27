b639c5f62a546f32590592df4fb753bd82ed31ca
KiarashTaheri <90288171+KiarashTaheri@users.noreply.github.com>
2023-06-27 10:47:04 -0700

    Update directoryLoop.py

--- directoryLoop.py
+++ directoryLoop.py
@@ -11,30 +11,32 @@ import os
 
 
 #Set Directory to iterate through
-directory = sys.argv[1]
+directory = (sys.argv[1])
+
 keyList = []
-header = ["PATH", "Echo Time", 'Repetition Time' , 'Study Date', 'Study Time']
+header = ["PATH", "AST #", "Directory","Echo Time", 'Repetition Time' , 'Study Date', 'Study Time']
 #files = [f for f in glob('/ISILON/home/kierasht/**', recursive=True) if dcm.dcmread(f)]
 #print(files)
-#for dirpath, dirnames, filenames in os.walk(directory, topdown=True, followlinks= True):
 
-    #for dicom in filenames:
-        #filelist = (dicom.partition(".")[0])
-for files in os.path.join(directory):
-    if dcm.dcmread(files):
-        print("yes")
-        #ds = dcm.dcmread(dicom)
-        #path = os.path.abspath(os.getcwd())
-        #keyInfo = [path, ds['EchoTime'].value, ds['RepetitionTime'].value , ds['StudyDate'].value, ds['StudyTime'].value]
-       #keyList.append(keyInfo)
+for path, directories, allFiles in os.walk(directory, topdown=True, followlinks= True):
+        for file in allFiles:
+            dicoms = os.path.join(path, file)
+            try:
+                ds = dcm.dcmread(dicoms)
+                specificPATH = os.path.abspath(ds)
+                if 'EchoTime' in ds and 'RepetitionTime' in ds and 'StudyDate' in ds and 'StudyTime' in ds:
+                    keyInfo = [specificPATH, specificPATH.split('/')[5], specificPATH.split('/')[6], ds['EchoTime'].value, ds['RepetitionTime'].value , ds['StudyDate'].value, ds['StudyTime'].value]
+                    keyList.append(keyInfo)
+                else:
+                    print("Not enough INFO")
+            except Exception:
+                print("Not a DICOM")
 
-'''
+
 if open("/ISILON/home/kierasht/astKeyData.csv", "w"):
     file = open("/ISILON/home/kierasht/astKeyData.csv", "w")
     writer = csv.writer(file)
-    print(keyList)
     writer.writerow(header)
     for row in keyList:
         writer.writerow(row)
     file.close
-    '''
